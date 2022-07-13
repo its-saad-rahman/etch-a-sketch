6a588e5ff462eebbd14634b0903844ac4401b008
Saad Rahman <itssaadrahman@gmail.com>
2022-07-13 10:23:16 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+//make a grid layout
+function makeGrid(row, col) {}
+//add click to draw
+//add a slider to choose grid layout
+//add custom color picker
+//add grid line toggler button
+//add clear button
