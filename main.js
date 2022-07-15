ca706a5af2c7e39317684e75c760736d224b902b
Saad Rahman <itssaadrahman@gmail.com>
2022-07-15 10:27:04 +0600

    Added clear function

--- main.js
+++ main.js
@@ -1,59 +1,61 @@
 const drawingArea = document.querySelector('.drawing-area');
 const sliderValue = document.querySelector('.slider-value span');
-const inputSlider = document.querySelector('input[data-name="grid"]');
-let isDrawing = false;
+const inputSlider = document.querySelector('input[type="range"]');
+const clear = document.querySelector('.clear');
 
-inputSlider.oninput = () => {
-  let value = inputSlider.value;
-
-  sliderValue.textContent = value + ' x ' + value;
-  sliderValue.style.left = `${value}% `;
-  sliderValue.classList.add('show');
-};
-inputSlider.onblur = () => {
-  sliderValue.classList.remove('show');
-};
-function inputSliderValue() {
-  let value = inputSlider.value;
-
-  return value;
-}
-console.log(inputSliderValue());
-inputSlider.addEventListener('input', inputSliderValue);
-// inputSlider.addEventListener('mousemove', inputSliderValue);
 //make a grid layout
-function makeGrid(rows, cols) {
+function makeGrid(values = 16) {
+  removeGrid();
   drawingArea.style.display = 'grid';
-  drawingArea.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
-  drawingArea.style.gridTemplateRows = `repeat(${cols}, 1fr)`;
+  drawingArea.style.gridTemplateColumns = `repeat(${values}, 1fr)`;
+  drawingArea.style.gridTemplateRows = `repeat(${values}, 1fr)`;
   drawingArea.style.gap = 0;
-  //drawingArea.style.border = `2px solid red`;
-}
-makeGrid(16, 16);
-
-function createDivElement(className, num) {
-  for (let i = 1; i <= num; i++) {
-    let div = document.createElement('div');
-    drawingArea.appendChild(div);
-    div.classList.add(`${className}`);
-    div.style.borderBottom = `1px solid black`;
-    div.style.borderRight = `1px solid black`;
-    // div.style.border = `1px solid black`;
+  let cell;
+  for (let i = 1; i <= values * values; i++) {
+    cell = document.createElement('div');
+    cell.classList.add('divArea');
+    cell.style.borderBottom = `1px solid #bdbdbd`;
+    cell.style.borderRight = `1px solid #bdbdbd`;
+    drawingArea.appendChild(cell);
+    cell.addEventListener('mousemove', draw);
   }
 }
-createDivElement('divArea', 256);
-const divDraw = document.querySelectorAll('.divArea');
 
 //add click to draw
 function draw(e) {
-  //if (!isDrawing) return;
-  console.log('clicked');
   e.target.style.background = 'black';
 }
 
-//divDraw.forEach((div) => div.addEventListener('mousedown', draw));
-//divDraw.forEach((div) => div.addEventListener('mousemove', draw));
 //add a slider to choose grid layout
+function showSliderPopup() {
+  let value = inputSlider.value;
+  sliderValue.textContent = value + ' x ' + value;
+  sliderValue.style.left = `${value}% `;
+  sliderValue.classList.add('show');
+
+  makeGrid(value);
+}
+
+function removeSliderPopup() {
+  sliderValue.classList.remove('show');
+}
+function removeGrid() {
+  while (drawingArea.firstChild) {
+    drawingArea.removeChild(drawingArea.lastChild);
+  }
+}
+
 //add custom color picker
 //add grid line toggler button
 //add clear button
+function clearBoard() {
+  let gridCells = drawingArea.querySelectorAll('div');
+  gridCells.forEach((gridCell) => (gridCell.style.background = 'white'));
+}
+
+const divDraw = document.querySelectorAll('.divArea');
+//All event listener
+
+inputSlider.addEventListener('input', showSliderPopup);
+clear.addEventListener('click', clearBoard);
+console.log(divDraw);
