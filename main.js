32fcef63ab9e9ea832798f91dd6e26d2a41bafcc
Saad Rahman <itssaadrahman@gmail.com>
2022-07-16 11:31:07 +0600

    Added color picker

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const sliderValue = document.querySelector('.slider-value span');
 const inputSlider = document.querySelector('input[type="range"]');
 const clear = document.querySelector('.clear');
 const randomClr = document.querySelector('.random-color');
+const rainbowClr = document.querySelector('.rainbow-color');
+const colorPicker = document.querySelector('#color-picker');
+
+let gridSize = 16;
 let backgroundColor = 'black';
 
 //make a grid layout
-function makeGrid(values = 16) {
+function makeGrid(values) {
   removeGrid();
   drawingArea.style.display = 'grid';
   drawingArea.style.gridTemplateColumns = `repeat(${values}, 1fr)`;
@@ -19,6 +23,7 @@ function makeGrid(values = 16) {
     cell.style.borderBottom = `1px solid #bdbdbd`;
     cell.style.borderRight = `1px solid #bdbdbd`;
     drawingArea.appendChild(cell);
+
     cell.addEventListener('mousemove', draw);
   }
 }
@@ -26,16 +31,17 @@ function makeGrid(values = 16) {
 //add click to draw
 function draw(e) {
   e.target.style.background = backgroundColor;
+  e.target.style.border = backgroundColor;
 }
 
 //add a slider to choose grid layout
 function showSliderPopup() {
-  let value = inputSlider.value;
-  sliderValue.textContent = value + ' x ' + value;
-  sliderValue.style.left = `${value}% `;
+  let gridSize = inputSlider.value;
+  sliderValue.textContent = gridSize + ' x ' + gridSize;
+  sliderValue.style.left = `${gridSize}% `;
   sliderValue.classList.add('show');
 
-  makeGrid(value);
+  makeGrid(gridSize);
 }
 
 function removeSliderPopup() {
@@ -48,34 +54,32 @@ function removeGrid() {
 }
 
 //add custom color picker
+function pickColor() {
+  backgroundColor = colorPicker.value;
+}
 // Generate Random color
-function randomHue() {
-  const random = Math.floor(Math.random() * 360) + 1;
-  return random;
+function generateRandomColor(e) {
+  const hue = Math.floor(Math.random() * 360) + 1;
+  const saturation = Math.floor(Math.random() * 100) + 1;
+  const luminance = Math.floor(Math.random() * 100) + 1;
+  backgroundColor = `hsl(${hue}, ${saturation}%, ${luminance}%)`; //, %
 }
-function randomSaturation() {
-  const random = Math.floor(Math.random() * 100) + 1;
-  return random;
-}
-function randomLuminance() {
-  const random = Math.floor(Math.random() * 100) + 1;
-  return random;
-}
-function generateRandomColor() {
-  backgroundColor = `hsl(${randomHue()}, ${randomSaturation()}%, ${randomLuminance()}%)`;
-}
-console.log(generateRandomColor());
-//add grid line toggler button
+
 //add clear button
 function clearBoard() {
   let gridCells = drawingArea.querySelectorAll('div');
-  gridCells.forEach((gridCell) => (gridCell.style.background = 'white'));
+  gridCells.forEach((gridCell) => {
+    gridCell.style.background = 'white';
+    gridCell.style.borderBottom = `1px solid #bdbdbd`;
+    gridCell.style.borderRight = `1px solid #bdbdbd`;
+  });
 }
 
-const divDraw = document.querySelectorAll('.divArea');
-//All event listener
+//Init default grid
+makeGrid(gridSize);
 
+//All event listener
 inputSlider.addEventListener('input', showSliderPopup);
 clear.addEventListener('click', clearBoard);
 randomClr.addEventListener('click', generateRandomColor);
-console.log(divDraw);
+colorPicker.addEventListener('input', pickColor);
